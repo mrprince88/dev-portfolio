fdc58782c40d31ea4f2d6a86ce71f2a8c485f3be
mrprince88 <mrprince0088@gmail.com>
2024-01-27 10:37:05 +0530

    fix: button ref

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -46,29 +46,35 @@ export interface AnchorButtonProps
   asChild?: boolean;
 }
 
-const AnchorButton = React.forwardRef<HTMLAnchorElement, AnchorButtonProps>(
-  ({ className, variant, size, asChild = false, ...props }) => {
-    const Comp = asChild ? Slot : "a";
+const AnchorButton = ({
+  className,
+  variant,
+  size,
+  asChild = false,
+  ...props
+}: AnchorButtonProps) => {
+  const Comp = asChild ? Slot : "a";
+  return (
+    <Comp
+      className={cn(buttonVariants({ variant, size, className }))}
+      {...props}
+    />
+  );
+};
+
+const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ className, variant, size, asChild = false, ...props }, ref) => {
+    const Comp = asChild ? Slot : "button";
     return (
       <Comp
         className={cn(buttonVariants({ variant, size, className }))}
+        ref={ref}
         {...props}
       />
     );
   },
 );
 
-const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ className, variant, size, asChild = false, ...props }) => {
-    const Comp = asChild ? Slot : "button";
-    return (
-      <Comp
-        className={cn(buttonVariants({ variant, size, className }))}
-        {...props}
-      />
-    );
-  },
-);
 Button.displayName = "Button";
 
 export { Button, buttonVariants, AnchorButton };
