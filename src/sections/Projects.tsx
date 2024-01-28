e0ec8e261eb33fd8ae24541f1ae166824d663760
mrprince88 <mrprince0088@gmail.com>
2024-01-28 16:31:47 +0530

    fix: spacing

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -13,7 +13,7 @@ export default function Projects() {
   return (
     <section className="my-20">
       <Fade duration={1000} direction="down" triggerOnce>
-        <h1 className="text-4xl font-bold">Projects</h1>
+        <h1 className="my-5 text-4xl font-bold">Projects</h1>
         <p className="mb-8 text-2xl">
           Here are some of the projects I've worked on recently
         </p>
