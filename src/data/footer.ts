ceffa7b75f38bd520e8b6974b6d974c4c6e227a2
mrprince88 <mrprince0088@gmail.com>
2024-01-28 19:04:58 +0530

    fix: footer

--- src/data/footer.ts
+++ src/data/footer.ts
@@ -4,4 +4,5 @@ export const footer = {
     "Discuss a project or just want to say hi? My inbox is open for all.",
   location: "Agra, India",
   openToOpportunities: "Yes",
+  phone: "+91 8077978214",
 };
--- src/sections/Profile.tsx
+++ src/sections/Profile.tsx
@@ -1,41 +1,37 @@
 import { Fade } from "~/components/ReactReveal";
-import { footer } from "~/data";
 import SocialHandles from "~/components/SocialHandles";
+import { IoLocationOutline } from "react-icons/io5";
+import { FaPhoneAlt } from "react-icons/fa";
+
+import { footer } from "~/data";
 
 export default function Profile() {
   return (
-    <section className="pb-20 pt-20 leading-[1.1] md:pl-20 md:pr-20">
+    <section className="pb-20 pt-20 leading-[1.1]">
       <Fade duration={1000} triggerOnce>
         <h1 className="text-4xl font-semibold">{footer.title}</h1>
-        <div className="flex justify-between">
-          <div className="mt-5 flex flex-col gap-2">
-            <p className="text-xl font-normal uppercase">{footer.subtitle}</p>
-            <p className="text-xl">
-              <svg
-                viewBox="-0.5 -2 20 19"
-                version="1.1"
-                width="22"
-                height="16"
-                aria-hidden="true"
-                stroke="currentColor"
-                className="mb-1 mr-1 inline-block"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M6 0C2.69 0 0 2.5 0 5.5 0 10.02 6 16 6 16s6-5.98 6-10.5C12 2.5 9.31 0 6 0zm0 14.55C4.14 12.52 1 8.44 1 5.5 1 3.02 3.25 1 6 1c1.34 0 2.61.48 3.56 1.36.92.86 1.44 1.97 1.44 3.14 0 2.94-3.14 7.02-5 9.05zM8 5.5c0 1.11-.89 2-2 2-1.11 0-2-.89-2-2 0-1.11.89-2 2-2 1.11 0 2 .89 2 2z"
-                ></path>
-              </svg>
-              <span>{footer.location}</span>
-            </p>
+        <div className="flex flex-col justify-between">
+          <div className="order-2 mt-5 flex flex-col gap-2">
+            <p className="text-2xl font-normal uppercase">{footer.subtitle}</p>
+            <span className="my-5">
+              <span className="text-xl">
+                <IoLocationOutline className="mb-1 mr-2 inline-block" />
+                <span>{footer.location}</span>
+              </span>
+              <span className="ml-4 text-xl">
+                <FaPhoneAlt className="mb-1 mr-2 inline-block" />
+                <span>{footer.phone}</span>
+              </span>
+            </span>
             <SocialHandles />
-            <p className="text-xl">
+            <p className="my-8 text-xl">
               Open to opportunities:{" "}
               <span className="text-blue-400">
                 {footer.openToOpportunities}
               </span>
             </p>
           </div>
-          <div className="ml-10 max-[768px]:hidden">
+          <div className="order-1 max-[768px]:my-10 md:ml-10">
             <img
               src="/assets/images/profile-picture.png"
               alt="Contact"
