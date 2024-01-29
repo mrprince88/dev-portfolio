d7d262a67e14b23cb89e6264c433669244c468fb
mrprince88 <mrprince0088@gmail.com>
2024-01-29 15:00:33 +0530

    fix: switch styles

--- src/components/ui/switch.tsx
+++ src/components/ui/switch.tsx
@@ -17,7 +17,7 @@ const Switch = React.forwardRef<
 >(({ className, thumbIcon, ...props }, ref) => (
   <SwitchPrimitives.Root
     className={cn(
-      "peer inline-flex h-6 w-14 shrink-0 cursor-pointer items-center rounded-full border-2 border-transparent shadow-sm transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 focus-visible:ring-offset-background disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary data-[state=unchecked]:bg-input",
+      "peer inline-flex h-6 w-16 shrink-0 cursor-pointer items-center rounded-full border-2 border-transparent shadow-sm shadow-foreground transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 focus-visible:ring-offset-background disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary data-[state=unchecked]:bg-input",
       className,
     )}
     {...props}
@@ -25,7 +25,7 @@ const Switch = React.forwardRef<
   >
     <SwitchPrimitives.Thumb
       className={cn(
-        "pointer-events-none flex h-7 w-7 items-center justify-center rounded-full border-[1px] border-foreground bg-background shadow-lg ring-0 transition-transform data-[state=checked]:translate-x-8 data-[state=unchecked]:translate-x-0",
+        "pointer-events-none flex h-7 w-7 items-center justify-center rounded-full bg-background shadow-sm shadow-foreground ring-0 transition-transform data-[state=checked]:translate-x-10 data-[state=unchecked]:translate-x-[-2px]",
       )}
     >
       {thumbIcon}
