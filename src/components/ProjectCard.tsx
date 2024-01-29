bd36918c34a4b7842f8a5fb5362cdad7f353fe45
mrprince88 <mrprince0088@gmail.com>
2024-01-29 11:00:14 +0530

    fix: hydration

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -7,7 +7,6 @@ import {
 } from "~/components/ui/card";
 import { AnchorButton } from "~/components/ui/button";
 import { Badge } from "~/components/ui/badge";
-import Link from "next/link";
 
 export default function ProjectCard({
   projectData,
@@ -23,39 +22,37 @@ export default function ProjectCard({
 }) {
   return (
     <Card className="h-full">
-      <Link href={projectData?.projectUrl} target="_blank">
-        <CardHeader className="items-center justify-center">
-          <img
-            alt={projectData?.projectName}
-            src={projectData?.projectImage}
-            className="w-fill h-96 object-cover"
-          />
-        </CardHeader>
-        <CardTitle>{projectData?.projectName}</CardTitle>
-        <CardContent>
-          {projectData?.projectDesc}
-          <div className="my-5 flex flex-wrap gap-5">
-            {projectData?.tags.map((tag) => (
-              <Badge key={`${projectData.projectName}${tag}`}>{tag}</Badge>
-            ))}
-          </div>
-        </CardContent>
-        <CardFooter className="mt-auto">
-          <AnchorButton
-            variant="secondary"
-            className="mr-5"
-            href={projectData?.projectUrl}
-          >
-            See Live
-          </AnchorButton>
-          <AnchorButton
-            variant={projectData?.codeLink ? "default" : "disabled"}
-            href={projectData?.codeLink}
-          >
-            See Code
-          </AnchorButton>
-        </CardFooter>
-      </Link>
+      <CardHeader className="items-center justify-center">
+        <img
+          alt={projectData?.projectName}
+          src={projectData?.projectImage}
+          className="w-fill h-96 object-cover"
+        />
+      </CardHeader>
+      <CardTitle>{projectData?.projectName}</CardTitle>
+      <CardContent>
+        {projectData?.projectDesc}
+        <div className="my-5 flex flex-wrap gap-5">
+          {projectData?.tags.map((tag) => (
+            <Badge key={`${projectData.projectName}${tag}`}>{tag}</Badge>
+          ))}
+        </div>
+      </CardContent>
+      <CardFooter className="mt-auto">
+        <AnchorButton
+          variant="secondary"
+          className="mr-5"
+          href={projectData?.projectUrl}
+        >
+          See Live
+        </AnchorButton>
+        <AnchorButton
+          variant={projectData?.codeLink ? "default" : "disabled"}
+          href={projectData?.codeLink}
+        >
+          See Code
+        </AnchorButton>
+      </CardFooter>
     </Card>
   );
 }
