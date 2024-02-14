dd37958be84e836760bebefee411ece81cda6a93
mrprince88 <mrprince0088@gmail.com>
2024-02-14 15:12:22 +0530

    chore: add catch all route

--- next.config.js
+++ next.config.js
@@ -5,16 +5,6 @@
 await import("./src/env.js");
 
 /** @type {import("next").NextConfig} */
-const config = {
-  redirects: async () => {
-    return [
-      {
-        source: "/:slug",
-        destination: "/",
-        permanent: true,
-      },
-    ];
-  },
-};
+const config = {};
 
 export default config;
