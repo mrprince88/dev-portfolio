e76cf0b7a1e19db2144c58397bfc7a849fad14fb
mrprince88 <mrprince008@gmail.com>
2024-11-28 10:59:24 +0530

    fix: styles on experience cards to fill available space

--- src/components/ExperienceCard.tsx
+++ src/components/ExperienceCard.tsx
@@ -25,42 +25,48 @@ export default function ExperienceCard({
   experienceData,
 }: ExperienceCardProps) {
   return (
-    <Card>
-      <Link href={experienceData?.url} target="_blank">
+    <Card className="flex">
+      <Link
+        href={experienceData?.url}
+        target="_blank"
+        className="flex flex-grow flex-col"
+      >
         <img
           alt={experienceData?.companyName}
           src={experienceData?.src}
           loading="lazy"
           className={`h-[280px] w-full rounded-t-xl object-contain ${experienceData?.companyName === "Firewires OneIOT" ? "bg-black" : "bg-white"}`}
         />
-        <CardHeader className="text-center">
-          <h2 className="text-2xl font-bold">{experienceData?.role}</h2>
-          <p className="text-xl">{experienceData?.companyName}</p>
-          <p className="text-xl">{experienceData?.duration}</p>
-        </CardHeader>
-        <CardContent className="whitespace-pre-line text-lg">
-          {experienceData?.description}
-          <ul className="list-disc pl-5">
-            {experienceData?.descBullets?.map((desc, index) => (
-              <li key={`${experienceData?.companyName}${index}`}>{desc}</li>
-            ))}
-          </ul>
-        </CardContent>
-        <CardFooter className="justify-start">
-          <div className="text-xl">
-            Tech stack
-            <div>
-              {experienceData?.techStack?.map((stack) => (
-                <Badge
-                  key={`${experienceData?.companyName}${stack}`}
-                  className="mr-2"
-                >
-                  {stack}
-                </Badge>
+        <div className="grid flex-grow">
+          <CardHeader className="text-center">
+            <h2 className="text-2xl font-bold">{experienceData?.role}</h2>
+            <p className="text-xl">{experienceData?.companyName}</p>
+            <p className="text-xl">{experienceData?.duration}</p>
+          </CardHeader>
+          <CardContent className="whitespace-pre-line text-lg">
+            {experienceData?.description}
+            <ul className="list-disc pl-5">
+              {experienceData?.descBullets?.map((desc, index) => (
+                <li key={`${experienceData?.companyName}${index}`}>{desc}</li>
               ))}
+            </ul>
+          </CardContent>
+          <CardFooter className="justify-start align-bottom">
+            <div className="text-xl">
+              Tech stack
+              <div>
+                {experienceData?.techStack?.map((stack) => (
+                  <Badge
+                    key={`${experienceData?.companyName}${stack}`}
+                    className="mr-2"
+                  >
+                    {stack}
+                  </Badge>
+                ))}
+              </div>
             </div>
-          </div>
-        </CardFooter>
+          </CardFooter>
+        </div>
       </Link>
     </Card>
   );
