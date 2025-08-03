e757ebaff01da0a3bb17163e2a7823bb074f4c99
mrprince88 <mrprince0088@gmail.com>
2025-08-03 17:05:18 +0530

    fix: animations

--- src/sections/Experience.tsx
+++ src/sections/Experience.tsx
@@ -1,12 +1,12 @@
 import ExperienceCard from "~/components/ExperienceCard";
-import { Slide } from "~/components/ReactReveal";
+import { Fade, Slide } from "~/components/ReactReveal";
 import { experienceInfo } from "~/data";
 
 export default function Experience() {
   return (
     <section className="py-20" id="experience">
       <h1 className="mb-8 text-3xl font-bold sm:text-4xl">Experience</h1>
-      <Slide duration={1000} direction="left" triggerOnce>
+      <Fade duration={500} direction="up" triggerOnce>
         <div className="grid-row grid grow-0 grid-cols-1 gap-5 sm:auto-rows-fr sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3">
           {experienceInfo.map((experience) => (
             <ExperienceCard
@@ -15,7 +15,7 @@ export default function Experience() {
             />
           ))}
         </div>
-      </Slide>
+      </Fade>
     </section>
   );
 }
